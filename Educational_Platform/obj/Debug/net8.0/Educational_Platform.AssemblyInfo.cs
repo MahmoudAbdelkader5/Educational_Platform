8f418032ada7f28d1af3649f7d1d422ebce59d61
mahmoud <mm4084@fayoum.edu.eg>
2025-04-19 13:47:18 +0200

    add accountMOdel

--- Educational_Platform/obj/Debug/net8.0/Educational_Platform.AssemblyInfo.cs
+++ Educational_Platform/obj/Debug/net8.0/Educational_Platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Educational_Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5077514ec719aaf14912feb01c8faca82327b884")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c357e31102487d6bafad73869486f553127eed19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Educational_Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Educational_Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
